87fe51b95f97fb088cc13fc6b576fccf9acbf84b
Nlenjibi Timothy <nlenjibitimo@gmail.com>
2026-01-11 19:37:07 +0000

    com

---
+++ properties/utils.py
@@ -0,0 +1,9 @@
+from django.core.cache import cache
+from .models import Property
+
+def get_all_properties():
+    properties = cache.get('all_properties')
+    if properties is None:
+        properties = list(Property.objects.all().values('id', 'title', 'description', 'price', 'location', 'created_at'))
+        cache.set('all_properties', properties, 3600)
+    return properties
--- properties/views.py
+++ properties/views.py
@@ -1,10 +1,10 @@
 
 from django.http import JsonResponse
 from django.views.decorators.cache import cache_page
-from .models import Property
+
+from .utils import get_all_properties
 
 @cache_page(60 * 15)
 def property_list(request):
-	properties = Property.objects.all().values('id', 'title', 'description', 'price', 'location', 'created_at')
-	data = list(properties)
+	data = get_all_properties()
 	return JsonResponse({'properties': data})
